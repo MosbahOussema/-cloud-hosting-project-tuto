e37f063a60dcf9d3e87b8f822993a542605a204f
oussema Mosbah <oussama.mosbah@astrolab-agency.com>
2024-09-12 11:02:37 +0100

    add CloudImage

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Image from "next/image";
+import CloudImage from "../../../public/cloud-hosting.png";
 
 const AboutPage = () => {
-  return <div>About Page</div>;
+  return (
+    <section className="fix-height countainer m-auto">
+      <h1 className="text-3xl font-bold text-gray-800 p-5"> About Page</h1>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus ipsam
+        nulla id libero sint laudantium quam ab autem accusantium consectetur
+        nesciunt in earum, deleniti rem, aspernatur recusandae sit dolorum
+        numquam.
+      </p>
+      <div>
+        <Image src={CloudImage} alt="cloud" width={500} height={500} />
+      </div>
+    </section>
+  );
 };
 
 export default AboutPage;
